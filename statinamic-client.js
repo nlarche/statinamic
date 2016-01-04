793649efa1034df0f50bbd604d83ca858ac6bec7
travis@localhost <travis@localhost>
2016-01-04 21:36:38 +0000

    publish: Docs: add overflow auto to code block
    generated from commit 58d5423d588833562809f60cc70858f4d98b75e2

--- statinamic-client.js
+++ statinamic-client.js
@@ -22556,7 +22556,7 @@
 	'use strict';
 	module.exports = function (str) {
 		return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
-			return '%' + c.charCodeAt(0).toString(16);
+			return '%' + c.charCodeAt(0).toString(16).toUpperCase();
 		});
 	};
 
